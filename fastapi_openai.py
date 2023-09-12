d254ac4cb71073e682922ec794dd96f3aa903726
Linyu12 <linyu@gapp.nthu.edu.tw>
2023-09-12 23:36:32 +0800

    can run

--- fastapi_openai.py
+++ fastapi_openai.py
@@ -24,20 +24,19 @@ load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 openai.api_key = api_key
 
-while True:
-    @app.post("/items/")
-    def create_item(msg: str ):
-        print(f"Received msg: {msg}")
-        try:
-            response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=msg,
-            max_tokens=128,
-            temperature=0.5,
-            )
-            completes_txt = response["choices"][0]["text"]
-            print(completes_txt)
-            return {"message": f"receive: {msg} and response: {completes_txt}"}
-        except Exception as e:
-            return {"error": str(e)}
+#while True: 
+@app.post("/items/")
+def create_item(msg: str ):
+    try:
+        response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=msg,
+        max_tokens=128,
+        temperature=0.5,
+        )
+        completes_txt = response["choices"][0]["text"]
+        print(completes_txt)
+        return {"message": f"receive: {msg} and response: {completes_txt}"}
+    except Exception as e:
+        return {"error": str(e)}
     
