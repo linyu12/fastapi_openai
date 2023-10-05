13cd27bede6c67083f0c889db452fb72f004dae2
Linyu12 <linyu@gapp.nthu.edu.tw>
2023-10-05 16:05:31 +0800

    function call

--- fastapi_openai.py
+++ fastapi_openai.py
@@ -12,6 +12,8 @@ from fastapi.middleware.cors import CORSMiddleware
 # uvicorn fastapi_openai:app --reload
 # http://localhost:8000/docs#/
 
+# pip install --upgrade google-api-python-client  //google-auth-httplib2 google-auth-oauthlib
+
 app = FastAPI()
 '''app.add_middleware(
     CORSMiddleware,
@@ -24,19 +26,33 @@ load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 openai.api_key = api_key
 
-#while True: 
-@app.post("/items/")
-def create_item(msg: str ):
+def ai_response(msg):
     try:
+        msg = msg
+        prompt1 = f'''
+            Please extract the following information from the given text and return it as a JSON object:
+
+            weather
+            location    
+
+            This is the body of text to extract the information from:
+            {msg}
+        '''
         response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=msg,
-        max_tokens=128,
-        temperature=0.5,
+        model = "text-davinci-003",
+        prompt = prompt1,
+        max_tokens = 128,
+        temperature = 0.5,
         )
+        
         completes_txt = response["choices"][0]["text"]
         print(completes_txt)
         return {"message": f"receive: {msg} and response: {completes_txt}"}
     except Exception as e:
         return {"error": str(e)}
+
+#while True: 
+@app.post("/items/")
+def create_item(msg: str ):
+    return ai_response(msg)
     
